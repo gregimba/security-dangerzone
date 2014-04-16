b3e7f7d1a3815570a42730133b1e8522ecb913b9
Grant Regimbal <Regimbal.grant@gmail.com>
2014-04-16 12:55:53 -0700

    building out the gui for pswd

--- pswd/app.py
+++ pswd/app.py
@@ -1,6 +1,8 @@
+from Tkinter import *
+
 try:
     import cpickle as pickle
-else:
+except:
     import pickle
 
 db = []
@@ -10,3 +12,36 @@ def load():
 
 def dump():
     pickle.dump(db,open("db.pkl","wb"))
+
+
+class Application(Frame):
+    def say_hi(self):
+        print "hi there, everyone!"
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.SAVE = Button(self)
+        self.SAVE["text"] = "SAVE"
+        self.SAVE["fg"] = "black"
+        self.SAVE["command"] = self.save
+
+        self.SAVE.pack({"side":"right"})
+
+    def save(self):
+    	pickle.dump(db,open("db.pkl","wb"))
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
